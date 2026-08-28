95fc6bbbdb0bd3fc16921ebf7017ac901670c588
candyzp <166943815+candyzp@users.noreply.github.com>
2026-08-27 23:28:14 -0400

    Support green toggle gravity portal

--- gd-sim/src/Objects/GravityPortal.cpp
+++ gd-sim/src/Objects/GravityPortal.cpp
@@ -1,16 +1,19 @@
 #include <Portals.hpp>
 #include <Player.hpp>
 
-GravityPortal::GravityPortal(Vec2D size, std::unordered_map<int, std::string>&& fields) : EffectObject(size, std::move(fields)), upsideDown(std::stoi(fields[1]) == 11) {}
-
+GravityPortal::GravityPortal(Vec2D size, std::unordered_map<int, std::string>&& fields) : EffectObject(size, std::move(fields)) {
+	int objectId = std::stoi(fields[1]);
+	mode = objectId == 11 ? 1 : (objectId == 2926 ? 2 : 0);
+}
 
 void GravityPortal::collide(Player& p) const {
 	EffectObject::collide(p);
 
-	if (upsideDown != p.upsideDown) {
-		// Gravity portals halve the velocity
+	bool target = mode == 2 ? !p.upsideDown : mode == 1;
+	if (target != p.upsideDown) {
+		// Gravity portals halve and reflect carried vertical velocity.
 		p.velocity = -p.velocity / 2;
-		p.upsideDown = upsideDown;
+		p.upsideDown = target;
 		p.gravityPortal = true;
 	}
 }
