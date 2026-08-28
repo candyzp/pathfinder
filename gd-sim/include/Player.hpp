0f71b815114bce70aa088874bfc53830fb0a4168
candyzp <166943815+candyzp@users.noreply.github.com>
2026-08-27 23:35:00 -0400

    Add gameplay-trigger state to player simulation

--- gd-sim/include/Player.hpp
+++ gd-sim/include/Player.hpp
@@ -8,7 +8,6 @@
 #include <functional>
 #include <optional>
 
-
 /// Player X velocity per speed
 inline double player_speeds[5] = {
 	251.16007972276924,
@@ -18,13 +17,12 @@ inline double player_speeds[5] = {
 	576.00020058307177
 };
 
-/// Used in player rotation. Similar to m_playerSpeed member variable
 inline float player_speedmults[5] = {
 	0.7,
 	0.9,
 	1.1,
 	1.3,
-	1.6	
+	1.6
 };
 
 double roundVel(double velocity, bool upsideDown);
@@ -33,9 +31,6 @@ struct Object;
 class Level;
 struct Slope;
 
-/**
- * The main player. This contains the entire player state and is the only thing that changes each frame.
- */
 struct Player : public Entity {
 	Vehicle vehicle;
 	Level* level;
@@ -48,28 +43,20 @@ struct Player : public Entity {
 	double robotBoostTime;
 	float dashAngle;
 	float dashSpeed;
+	float gravityScale;
+	float timeWarp;
+	int direction;
 
-	/// See util.hpp for what cow_set is
 	cow_set<int> usedEffects;
-
-	// Potential slopes are important for block collisions. Reset per frame.
 	std::vector<Slope const*> potentialSlopes;
-
-	/// Actions will be ran at the beginning of every frame.
 	std::vector<std::function<void(Player&)>> actions;
 
-	/// Slopes have special collision rules. See Slope.cpp for more information.
 	struct {
 		std::optional<Slope> slope;
-
-		/// Time on slope
 		double elapsed;
-
-		/// When colliding with a downhill slope with a positive velocity.
 		bool snapDown;
 	} slopeData;
 
-	/// X-snapping. See Block.cpp for more information
 	struct {
 		Entity object;
 		int playerFrame = 0;
@@ -79,49 +66,21 @@ struct Player : public Entity {
 	float floor;
 	float dt;
 
-	/// Some vehicles have coyote frames for valid inputs
 	unsigned int coyoteFrames;
-
 	int speed;
 	int frame;
 
 	bool dead;
 	bool grounded;
-
-	/**
-	 * Under normal circumstances, acceleration is applied to the velocity
-	 * at the end of a frame. When this field is set, acceleration will not
-	 * be applied at the end of the frame. Mainly set via `setVelocity`
-	 */
 	bool velocityOverride;
-
-	/**
-	 * `button` vs `input`: Button always refers to whether a click was applied; input can be disabled
-	 * for niche circumstances where no other normal operations are allowed despite a click.
-	 */
 	bool button, input;
-
-	/// If a click is being buffered. Used for things like orb clicks.
 	bool buffer;
-	/**
-	 * In the ball vehicle, holding a click while transitioning into another vehicle will cause a
-	 * buffered input, despite things like orbs not buffering in the same way.
-	 */
 	bool vehicleBuffer;
-
 	bool upsideDown;
 	bool small;
-
-	/// Entering a gravity portal can cause the next frame to have certain edge cases
 	bool gravityPortal;
-
-	/// Whether velocity at the end of the frame is to be rounded (typically used by ball)
 	bool roundVelocity;
-
-	/// Dash-orb state.
 	bool dashing;
-
-	/// Dual state is copied with each frame so rollback stays deterministic.
 	bool dualActive;
 	bool player2;
 
@@ -136,16 +95,12 @@ struct Player : public Entity {
 	void preCollision(bool input);
 	void postCollision();
 
-	
 	Entity unrotatedHitbox() const;
-
-	/// Inner hitbox of {9, 9} that is used mainly for blocks
 	Entity innerHitbox() const;
 
 	Player const& prevPlayer() const;
 	Player const* nextPlayer() const;
 
-	/// Values relative to player gravity.
 	template <typename T>
 	T grav(T value) const { return upsideDown ? -value : value; }
 	inline float gravBottom(Entity const& e) const { return upsideDown ? -e.getTop() : e.getBottom(); }
